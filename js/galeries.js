09d9f08d4b429469d982df0eb09ac44bfefb9057
Edijs <edijsapse@gmail.com>
2018-05-10 22:34:02 +0300

    Mainly added Image preview functionality

--- js/galeries.js
+++ js/galeries.js
@@ -1,10 +1,19 @@
-$(document).ready(function(){
-    $("img").click(function(){
-        var imgSrc = $(this).attr("src");
-        $("#img-full > img").attr("src", imgSrc);
-        $("#img-full").fadeIn();
-    });
-    $("#img-full").click(function(){
-        $(this).fadeOut();
-    })
+$(document).ready(function() {
+	$("img").click(function(){
+		var image_src = $(this).attr("src"),
+			image = document.createElement("img");
+		$("#preview").fadeIn("fast", function(){
+			$(image).attr("src", image_src);
+			$("#preview > #image").append(image);
+			$(image).fadeIn("fast", function(){
+				$("#close").fadeIn("slow");
+			});
+		});
+		$("#close").click(function(){
+			$("#preview").fadeOut(200, function(){
+				$("#image > img").remove();
+				$("#close").css("display", "none");
+			});
+		});
+	});
 });
